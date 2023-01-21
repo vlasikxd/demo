4d5240c88cf6c3f0696316da382b5a7644efe4bc
vlasikxd <nobel96@bk.ru>
2023-01-21 14:51:18 +0300

    changes 2

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,10 @@ import java.util.Scanner;
 
 public class Main {
 
-    private static boolean rome = true;
+    private static boolean rome;
 
     public static void main(String[] args) throws Exception {
+        rome = true;
         Scanner input_ = new Scanner(System.in);
         String input = input_.nextLine();
         System.out.println(calc(input));
@@ -14,14 +15,15 @@ public class Main {
         String[] res = input.split(" ");
 
         if (res.length > 3) {
-            throw new Exception("//т.к. формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)");
+            throw new Exception("формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)");
         }
         if (res.length < 3) {
             throw new Exception("строка не является математической операцией");
         }
-        int[] numbers = getNumbers(res[0], res[2]);
-        if (numbers[0] > 10 | numbers[1] > 10) {
-            throw new Exception("введенные числа должны быть от 1 до 10");
+
+        int[] numbers = getNumbers(res);
+        if (numbers[0] > 10 | numbers[1] > 10 | numbers[0] < 1 | numbers[1] < 1) {
+            throw new Exception("Используйте только арабские или римские числа от 1 до 10");
         }
 
         if (rome) {
@@ -30,19 +32,34 @@ public class Main {
             return String.valueOf(getResult(numbers, res[1]));
         }
     }
-    static int[] getNumbers(String num1, String num2) throws Exception {
-        try {
-            int[] numbers = {Integer.parseInt(num1), Integer.parseInt(num2)};
-            rome = false;
-            return numbers;
-        } catch (Exception a) {
+
+    static int[] getNumbers(String[] a) throws Exception {
+        int count = 0;
+        int[] result = new int[2];
+
+        for (int i = 0; i < 3; i += 2) {
             try {
-                return new int[]{Rome.valueOf(num1).getArabic(), Rome.valueOf(num2).getArabic()};
-            } catch (Exception b) {
-                throw new Exception("используются одновременно разные системы счисления");
+                result[count] = Integer.parseInt(a[i]);
+                count++;
+            } catch (Exception ignored) {
             }
         }
+
+        if (count == 1) {
+            throw new Exception("используются одновременно разные системы счисления");
+        }
+        if (count == 2) {
+            rome = false;
+            return result;
+        }
+
+        try {
+            return new int[]{Rome.valueOf(a[0]).getArabic(), Rome.valueOf(a[2]).getArabic()};
+        } catch (Exception b) {
+            throw new Exception("Используйте только арабские или римские числа от 1 до 10");
+        }
     }
+
     static String getRome(int number) {
         return "I".repeat(number)
                 .replace("IIIII", "V")
@@ -58,6 +75,7 @@ public class Main {
                 .replace("DD", "M")
                 .replace("DCD", "CM");
     }
+
     static int getResult(int[] numbers, String sign) throws Exception {
         int result = switch (sign) {
             case "+" -> numbers[0] + numbers[1];
@@ -68,7 +86,7 @@ public class Main {
                     throw new Exception("формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)");
         };
         if (rome & result < 1) {
-            throw new Exception("D римской системе нет отрицательных чисел");
+            throw new Exception("В римской системе нет отрицательных чисел");
         } else {
             return result;
         }
